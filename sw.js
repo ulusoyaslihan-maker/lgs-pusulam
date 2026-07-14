e93c584d8756c40fa2640e90e1f2715789939620
ulusoyaslihan-maker <ulusoyaslihan@gmail.com>
2026-07-14 15:56:03 +0300

    Program bugunden (13 Tem 2026) baslar: genel takvim her gun konu adiyla dolu, bos yaz gunu kalmadi; surum 12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LGS Pusulam — çevrimdışı çalışma için service worker */
-const CACHE = "lgs-pusulam-v11";
+const CACHE = "lgs-pusulam-v12";
 const CORE = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
